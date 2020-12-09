1488eb571beb917db5cd59d9548d7a14d42ca3a0
Mirko <mirko.rinelli@gmail.com>
2020-12-09 19:30:16 +0100

    JSnack E04S01
    Code fixed with ES6

--- main.js
+++ main.js
@@ -6,15 +6,9 @@
 /*let number1 = Number(prompt(`Inserisci un primo numero`));
 let number2 = Number(prompt(`Inserisci un secondo numero`));
 
-const printMaxNumber = (num1, num2) => {
-  if (num1 >num2) {
-    console.log(num1);
-  } else {
-    console.log(num2);
-  }
-}
+const printMaxNumber = (num1, num2) => `${num1 > num2 ? console.log(num1) : console.log(num2)}`;
 
- printMaxNumber(number1, number2);*/
+printMaxNumber(number1, number2);*/
 
 // E02S01
 //L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
@@ -22,24 +16,26 @@ const printMaxNumber = (num1, num2) => {
 /*let word1 = (prompt(`Inserisci la prima parola`));
 let word2 = (prompt(`Inserisci la seconda parola`));
 
-const longWord = (word1, word2) => {
-  if (word1.length > word2.length) {
-    console.log(word1);
-  } else if (word1.length == word2.length) {
-    console.log(`Le parole sono lunghe uguali`);
-  } else {
-    console.log(word2);
-  }
-}
+const longWord = (word1, word2) => `${word1.length > word2.length ? console.log(word1) : console.log(word2)}`;
 
 longWord (word1, word2);*/
 
 // E03S01
 //Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.
 
-let sum = 0;
+/*let sum = 0;
 for (let i = 0; i < 5; i++) {
   let userNumber = Number(prompt(`Inserisci un numero`));
-  sum = sum + userNumber;
-  console.log(sum);
+  console.log(`${sum = sum + userNumber}`);
+}*/
+
+
+// E04S01
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const userList = [`Mario Rossi`, `Luca Bianchi`, `Andrea Verdi`];
+
+for (let i = 0; i < userList.length; i++) {
+  let userName = prompt(`Inserisci il tuo nome e cognome`);
+  console.log(`${userName} ${userList.includes(userName) ? "può partecipare alla festa" : "è uno sfigato! Torna a casa" }`);
 }
